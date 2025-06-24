8973726d599f4e4a793114e4b5adbf009d2842b8
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-25 00:09:52 +1000

    Update m-mode.py

--- m-mode.py
+++ m-mode.py
@@ -3,5 +3,8 @@ import os
 start_monitor_mode = "sudo airmon-ng start wlan0"
 os.system(start_monitor_mode)
 
+kill_process = "sudo airmon-ng check kill"
+os.system(kill_process)
+
 auto_airodump = "sudo airodump-ng wlan0"
 os.system(auto_airodump)
