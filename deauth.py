23bf29ddf3f4dae2697da33dff4198b1fdf41751
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-24 23:53:02 +1000

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -1,7 +1,7 @@
 import os 
-
-os.system("sudo airmon-ng start wlan0")
 interface = "wlan0"
+os.system(f"sudo airmon-ng start {interface}")
+os.system(f"sudo airmon-ng check kill {interface}")
 ent_ch = input("enter your network channel: ")
 os.system(f"sudo iwconfig {interface} channel {ent_ch}")
 try: 
