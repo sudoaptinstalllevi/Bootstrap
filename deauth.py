35e03dc66e4cee4d7e5a3dde59df27bf0566867e
sudoaptinstalllevi <fcloudservice@gmail.com>
2025-06-25 11:31:21 +1000

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -1,6 +1,6 @@
 import os 
 interface = "wlan0"
-ent_ch = input("enter your network channel: ")
+ent_ch = input("enter your targets network channel: ")
 os.system(f"sudo iwconfig {interface} channel {ent_ch}")
 try: 
   ap = input("ap: ")
